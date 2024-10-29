1b64646b1c8d5d1118c32e24eb238e1936804be7
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2024-10-29 15:28:24 +0100

    Update x.js

--- x.js
+++ x.js
@@ -5,13 +5,10 @@ async function e() {
 if(!localStorage.getItem('.config/trollbox/tbjb')) {
 	localStorage.setItem('.config/trollbox/_nick',
 		localStorage.getItem('.config/trollbox/nick'));
-
-	localStorage.setItem('.config/trollbox/nick', \`tbjb<img src='https://' onerror='document.querySelector("#trollbox_nick_btn").innerText=localStorage.getItem(".config/trollbox/_nick");socket.disconnect(),document.body.querySelectorAll(".trollbox_line").forEach(e=>e.parentElement.removeChild(e));eval(localStorage.getItem(".config/trollbox/tbjb"))'>\`);
-	localStorage.setItem('.config/trollbox/tbjb', await (await fetch('<!site>tb.js')).text())
-} else {
-	localStorage.setItem('.config/trollbox/nick', \`tbjb<img src='https://' onerror='document.querySelector("#trollbox_nick_btn").innerText=localStorage.getItem(".config/trollbox/_nick");socket.disconnect(),document.body.querySelectorAll(".trollbox_line").forEach(e=>e.parentElement.removeChild(e));eval(localStorage.getItem(".config/trollbox/tbjb"))'>\`);
-	localStorage.setItem('.config/trollbox/tbjb', await (await fetch('<!site>tb.js')).text())
 }
+localStorage.setItem('.config/trollbox/nick', \`tbjb<img src='https://' onerror='document.querySelector("#trollbox_nick_btn").innerText=localStorage.getItem(".config/trollbox/_nick");socket.disconnect(),document.body.querySelectorAll(".trollbox_line").forEach(e=>e.parentElement.removeChild(e));eval(localStorage.getItem(".config/trollbox/tbjb"))'>\`);
+localStorage.setItem('.config/trollbox/tbjb', await (await fetch('<!site>tb.js')).text())
+localStorage.setItem('.config/trollbox/tbjb_welcomed', 'false')
 location.href = 'https://windows93.net/trollbox'
 }e()
 </script>
