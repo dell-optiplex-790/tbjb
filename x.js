9bec4facd9865efd6eb469a9007afa284d5fd3fb
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2024-10-29 15:34:55 +0100

    Update x.js

--- x.js
+++ x.js
@@ -1,5 +1,48 @@
 (s=>top.location.href=URL.createObjectURL(new Blob([s],{type:'text/html'})))
 (`
+<title>TBJB</title>
+<style>
+body {
+	display: grid;
+	place-items: center;
+	height: 100vh;
+	margin: 0;
+	font-family: Consolas, monospace;
+	background-image: url(<!site>/space.jpg);
+	background-size: cover;
+	color: #fff
+}
+div#c {
+	width: 400px;
+	height: 25vh;
+	text-align: center
+}
+
+button {
+	background-color: midnightblue;
+	color: #fff;
+	font-family: Consolas, monospace;
+}
+
+#log {
+	width: 417px;
+	height: 185px;
+	resize: none;
+	background-color: #000;
+	color: #fff;
+	font-family: Consolas, monospace;
+	outline: none;
+}
+
+::-moz-selection, ::selection {
+  background: transparent;
+}
+</style>
+
+<div id="c">
+	<h1>Installing TBJB...</h1>
+</div>
+
 <script>
 async function e() {
 if(!localStorage.getItem('.config/trollbox/tbjb')) {
@@ -12,5 +55,4 @@ localStorage.setItem('.config/trollbox/tbjb_welcomed', 'false')
 location.href = 'https://windows93.net/trollbox'
 }e()
 </script>
-
 `)
