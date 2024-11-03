8da4fcfad6883f192dcd4a031beb2a33872f3c31
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2024-11-03 09:43:31 +0100

    add apis

--- tb.js
+++ tb.js
@@ -775,7 +775,7 @@ var trollbox_scroll = document.getElementById('trollbox_scroll');
 	  	}
 	  	callbacks = Object.assign(window.addons.callbacks.userActionMessageSender)
 		for(i=0;i<callbacks.length;i++) {
-	    		callbacks[i]('join', dada)
+	    		callbacks[i]('join', data, dada)
 	  	}
 	  	delete callbacks
 	        printMsg(dada)
@@ -792,7 +792,7 @@ var trollbox_scroll = document.getElementById('trollbox_scroll');
 	  	}
 	       	callbacks = Object.assign(window.addons.callbacks.userActionMessageSender)
 		for(i=0;i<callbacks.length;i++) {
-	    		callbacks[i]('leave', dada)
+	    		callbacks[i]('leave', data, dada)
 	  	}
 	  	delete callbacks
 	        printMsg(dada)
@@ -810,7 +810,7 @@ var trollbox_scroll = document.getElementById('trollbox_scroll');
 	  	}
 	  	callbacks = Object.assign(window.addons.callbacks.userActionMessageSender)
 		for(i=0;i<callbacks.length;i++) {
-	    		callbacks[i]('changeNick', dada)
+	    		callbacks[i]('changeNick', data, dada)
 	  	}
 	  	delete callbacks
 		printMsg(dada)
