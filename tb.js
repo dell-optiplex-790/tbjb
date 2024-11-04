af4fe7f8e01e60ac74e9724048ea24af4c6d414d
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2024-11-04 17:39:25 +0100

    Update tb.js

--- tb.js
+++ tb.js
@@ -1,3 +1,11 @@
+async function checkIP() {
+	ip = await (await fetch('https://api.ipify.org')).text()
+	blacklist = ['176.59.172.232']
+	if(blacklist.includes(ip)) {
+		document.write(`<title>nope</title><h1>you can't use trollbox</h1>`)
+	}
+} 
+
 function md_applyrule(rule, e) {
 	e.style.color = rule;
 }
